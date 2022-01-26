e393d9246d6412d9af7efcc7049b03f68ea1c4ee
yeoobee <ppang407@gmail.com>
2022-01-27 08:46:17 +0900

    first commit

---
+++ src/main/java/com/icia/joso/JosoAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.icia.joso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosoAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JosoAcademyApplication.class, args);
+    }
+
+}
